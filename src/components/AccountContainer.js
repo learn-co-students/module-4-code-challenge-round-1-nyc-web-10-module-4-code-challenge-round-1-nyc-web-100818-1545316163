d20ddf2c0c7aac9f59dffe0fc51727fd9211fb5f
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-12-20 09:45:59 -0500

    initial state

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from 'react';
+import TransactionsList from './TransactionsList';
+import Search from './Search';
+import { transactions } from '../transactionsData';
 
 class AccountContainer extends Component {
+  // constructor() {
+  //   super()
+  //   this.state
 
-  constructor() {
-    super()
+  //   // get a default state working with the data imported from TransactionsData
+  //   // use this to get the functionality working
+  //   // then replace the default transactions with a call to the API
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+  // }
 
-  }
+  state = {
+    transactions: [{ transactions }]
+  };
 
-  handleChange(event) {
-    // your code here
-  }
+  handleChange = event => {
+    console.log(this.state);
+  };
 
   render() {
-
     return (
       <div>
         <Search />
         <TransactionsList />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
