2186149a80ce524cf1625d74b69cdb2893f5b07d
aaaaus <ausluft@gmail.com>
2018-12-20 10:12:40 -0500

    challenge completed 💸

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
+
+const API = "https://boiling-brook-94902.herokuapp.com/transactions"
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+  constructor(props) {
+    super(props)
+    this.state = {
+      transactions: [],
+      searchTerm: ''
+    }
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch(API).then(resp=>resp.json()).then(tData=> {
+      this.setState({
+        transactions: tData
+      })
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  searchFilter = () => {
+    return this.state.transactions.filter(t => {
+      return t.description.toLowerCase().includes(this.state.searchTerm.toLowerCase()) || t.category.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    })
   }
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search
+        searchTerm={this.state.searchTerm}
+        handleChange={this.handleChange}/>
+        <TransactionsList transactions={this.searchFilter()}/>
       </div>
     )
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+
+  // console.log("TLIST props are: ", props.transactions);
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +31,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {props.transactions.map(t=> <Transaction key={t.id} transaction={t} />)}
 
       </tbody>
     </table>
