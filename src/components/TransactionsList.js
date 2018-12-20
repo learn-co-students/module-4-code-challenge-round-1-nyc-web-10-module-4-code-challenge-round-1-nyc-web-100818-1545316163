739862fb1b00c5d28ac883551b07ce1d6790ef91
free2580 <fridakahsas@yahoo.com>
2018-12-20 10:26:53 -0500

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = ({ transactions }) => {
+
+  const renderTransactions = () => {
+    return transactions.map(trn => {
+      return <Transaction key={trn.id} transaction={trn} />
+    })
+  }
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +35,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {renderTransactions()}
 
       </tbody>
     </table>
