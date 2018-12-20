434f85c67640b8fc2af6f66d500aa002c5a397bf
Saajid Khan <saajidkhan@Saajids-MacBook-Pro.local>
2018-12-20 09:53:28 -0500

    weird flex but ok

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,27 +3,42 @@ import TransactionsList from './TransactionsList'
 import Search from './Search'
 import {transactions} from '../transactionsData'
 
+
+const API = "https://boiling-brook-94902.herokuapp.com/transactions"
+
+
 class AccountContainer extends Component {
-
-  constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+  state = {
+    data: [],
+    term: ""
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount(){
+    fetch(API)
+      .then(r => r.json())
+      .then(dataAPI => {
+        this.setState({data: dataAPI})
+      })
+  }
+
+  handleChange = (event) => {
+    this.setState({term: event.target.value})
+  }
+
+  filteredData = () => {
+    const newData = this.state.data.filter(data => {
+      return data.category.toLowerCase().includes(this.state.term.toLowerCase())
+      || data.description.toLowerCase().includes(this.state.term.toLowerCase())
+    })
+    return newData
   }
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList data={this.filteredData()}/>
       </div>
     )
   }
