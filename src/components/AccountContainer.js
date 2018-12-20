b4149175f6cb1ade772128865490cbafd95b23c3
Yoan Ante <yoanante@gmail.com>
2018-12-20 10:36:19 -0500

    If the code's in git you must commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,57 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
+const API = "https://boiling-brook-94902.herokuapp.com/transactions";
 
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      transactions: [],
+      search: ""
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch(API).then(resp => resp.json())
+      .then( transactions =>  this.setState({transactions}))
   }
+
+  handleChange = (event) =>  {
+    // console.log(this.state)
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  filteredTransactions = () => {
+    let newArray = []
+    newArray = this.state.transactions.filter(transaction => {
+      return (transaction.description
+          .toLowerCase()
+          .includes(
+            this.state.search.toLowerCase()
+          ) || transaction.description
+          .toLowerCase()
+          .includes(this.state.search.toLowerCase()));
+    })
+    return newArray
+  }
+  
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search search={this.handleChange}/>
+        {/* <TransactionsList transactions={this.state.transactions}/> */}
+        <TransactionsList transactions={this.filteredTransactions()}/>
       </div>
     )
   }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.transaction.posted_at}</td>
+      <td>{props.transaction.description}</td>
+      <td>{props.transaction.category}</td>
+      <td>{props.transaction.amount}</td>
     </tr>
   )
 }
 
 export default Transaction
+
+
+// { id: 1, posted_at: "2017-02-28 11:00:00", description: "Leather Pants, Gap co.", category: "Fashion", amount: -20000 }
